80bb86f074ba0868df55a9214e064cdd006d2eed
gowdack792001 <gowdack.xworkz@gmail.com>
2022-06-21 22:42:22 +0530

    commitin method files

---
+++ Addition.java
@@ -0,0 +1,18 @@
+class Addition{
+
+ public static void main (String a[]){
+ 
+ int finalValue = add(56,54);
+ System.out.println("finalValue");
+ 
+ }
+
+ public static int add (int a ,int b){
+ 
+ return a+b ;
+ }
+ public static void add (int a,int b,int c){
+ System.out.println("a+b+c") ;
+ }
+
+}
---
+++ Userlogin.java
@@ -0,0 +1,15 @@
+class Userlogin{
+
+
+public static void main ( String a[]){
+
+
+}
+ public static void login (long contactNo){
+ 
+ }
+ 
+ public static void login (String email){
+ System.out.println("logged in successfully");
+ }
+ }
